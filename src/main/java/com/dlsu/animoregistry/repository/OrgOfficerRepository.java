c4a76050f1d760b32d4126c5bda433fd58ed999f
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-17 00:48:09 +0800

    completed officer repository for org officer search #4

--- src/main/java/com/dlsu/animoregistry/repository/OrgOfficerRepository.java
+++ src/main/java/com/dlsu/animoregistry/repository/OrgOfficerRepository.java
@@ -1,4 +1,12 @@
 package com.dlsu.animoregistry.repository;
 
-public interface OrgOfficerRepository {
+import com.dlsu.animoregistry.model.OrgOfficer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrgOfficerRepository extends JpaRepository<OrgOfficer, Long> {
+    Optional<OrgOfficer> findByDlsuEmail(String dlsuEmail);
+    List<OrgOfficer> findByOrganizationId(Long organizationId);
 }
