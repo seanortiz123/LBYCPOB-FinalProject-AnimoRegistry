a1bd4aaa39a272390ef24b6804761f3db29e1e7e
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-16 22:10:23 +0800

    added base entity structure and fields for DLSUUser #2

--- src/main/java/com/dlsu/animoregistry/model/DLSUUser.java
+++ src/main/java/com/dlsu/animoregistry/model/DLSUUser.java
@@ -2,4 +2,14 @@ package com.dlsu.animoregistry.model;
 
 public abstract class DLSUUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String idNumber;
+    private String name;
+    private String dlsuEmail;
+
+    @JsonIgnore
+    private String password;
 }
