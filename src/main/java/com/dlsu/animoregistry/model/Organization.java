5197337f1e8f8ba11e008b01574d47d7e91e41bd
Gian Fadriquela <gian_fadriquela@dlsu.edu.ph>
2026-08-16 20:01:40 +0800

    Added fields (id, name, category, description, logoUrl, socialMediaHandle) in Organization.java #8

--- src/main/java/com/dlsu/animoregistry/model/Organization.java
+++ src/main/java/com/dlsu/animoregistry/model/Organization.java
@@ -1,4 +1,13 @@
 package com.dlsu.animoregistry.model;
 
 public class Organization {
+
+    private Long id;
+
+    private String name;              // e.g. "LSCS", "animo.sys", "Green Media Group"
+    private String category;          // e.g. "Academic", "Special Interest", "Community Service"
+    private String description;
+    private String logoUrl;
+    private String socialMediaHandle;
+
 }
