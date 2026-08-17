9204c382c0792419f28e7dc120d5f199318e22a5
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-17 23:35:22 +0800

    Set up the organization officer service with fields and constructor #4

--- src/main/java/com/dlsu/animoregistry/service/OrgOfficerService.java
+++ src/main/java/com/dlsu/animoregistry/service/OrgOfficerService.java
@@ -1,4 +1,18 @@
 package com.dlsu.animoregistry.service;
 
+import com.dlsu.animoregistry.model.OrgOfficer;
+import com.dlsu.animoregistry.repository.OrgOfficerRepository;
+import com.dlsu.animoregistry.repository.OrganizationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrgOfficerService {
+
+    private final OrgOfficerRepository officerRepository;
+    private final OrganizationRepository organizationRepository;
+
+    public OrgOfficerService(OrgOfficerRepository officerRepository, OrganizationRepository organizationRepository) {
+        this.officerRepository = officerRepository;
+        this.organizationRepository = organizationRepository;
+    }
 }
