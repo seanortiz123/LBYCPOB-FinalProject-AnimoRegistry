1e7d48b1504f434b0e30515a3c3df9b65051a821
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-18 00:12:53 +0800

    added getters and setters for dlsuEmail and password #5

--- src/main/java/com/dlsu/animoregistry/dto/LoginRequest.java
+++ src/main/java/com/dlsu/animoregistry/dto/LoginRequest.java
@@ -5,5 +5,22 @@ public class LoginRequest {
     private String dlsuEmail;
     private String password;
 
+    public LoginRequest() {
+    }
 
+    public String getDlsuEmail() {
+        return dlsuEmail;
+    }
+
+    public void setDlsuEmail(String dlsuEmail) {
+        this.dlsuEmail = dlsuEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
