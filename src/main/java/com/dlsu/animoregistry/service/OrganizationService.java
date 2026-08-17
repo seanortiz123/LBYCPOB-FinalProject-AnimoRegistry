2efeee34769e01bbe5a0c6550cd0c5e49a0cf075
Gian Fadriquela <gian_fadriquela@dlsu.edu.ph>
2026-08-17 10:06:47 +0800

    Added methods in class OrganizationService.java #9

--- src/main/java/com/dlsu/animoregistry/service/OrganizationService.java
+++ src/main/java/com/dlsu/animoregistry/service/OrganizationService.java
@@ -1,4 +1,31 @@
 package com.dlsu.animoregistry.service;
 
+import com.dlsu.animoregistry.model.Organization;
+import com.dlsu.animoregistry.repository.OrganizationRepository;
+
+import java.util.List;
+
 public class OrganizationService {
+
+    private final OrganizationRepository organizationRepository;
+
+    public OrganizationService(OrganizationRepository organizationRepository) {
+        this.organizationRepository = organizationRepository;
+    }
+
+    public Organization create(Organization organization) {
+        return organizationRepository.save(organization);
+    }
+
+    public List<Organization> getAll() {
+        return organizationRepository.findAll();
+    }
+
+    public List<Organization> getOpenForRegistration() {
+        return organizationRepository.findByRegistrationOpenTrue();
+    }
+
+    public List<Organization> getByCategory(String category) {
+        return organizationRepository.findByCategoryIgnoreCase(category);
+    }
 }
