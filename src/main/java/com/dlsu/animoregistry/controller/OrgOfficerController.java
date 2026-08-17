b743cfaeafb9c5988f73bb57cbee8ba516db05f7
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-18 01:39:27 +0800

    created class OrgOfficerController and added feat(officer): register and getAll endpoints #5

---
+++ src/main/java/com/dlsu/animoregistry/controller/OrgOfficerController.java
@@ -0,0 +1,30 @@
+package com.dlsu.animoregistry.controller;
+
+import com.dlsu.animoregistry.model.OrgOfficer;
+import com.dlsu.animoregistry.service.OrgOfficerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/officers")
+public class OrgOfficerController {
+
+    private final OrgOfficerService officerService;
+
+    public OrgOfficerController(OrgOfficerService officerService) {
+        this.officerService = officerService;
+    }
+
+    @PostMapping("/register/{organizationId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrgOfficer register(@PathVariable Long organizationId, @RequestBody OrgOfficer officer) {
+        return officerService.register(organizationId, officer);
+    }
+
+    @GetMapping
+    public List<OrgOfficer> getAll() {
+        return officerService.getAll();
+    }
+}
