c34339c025c05c20ac798be929be1326cf775949
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-17 23:50:39 +0800

    set up the student service with field and constructor #4

--- src/main/java/com/dlsu/animoregistry/service/StudentService.java
+++ src/main/java/com/dlsu/animoregistry/service/StudentService.java
@@ -1,4 +1,15 @@
 package com.dlsu.animoregistry.service;
 
+import com.dlsu.animoregistry.model.LasallianStudent;
+import com.dlsu.animoregistry.repository.LasallianStudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+    private final LasallianStudentRepository studentRepository;
+
+    public StudentService(LasallianStudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 }
