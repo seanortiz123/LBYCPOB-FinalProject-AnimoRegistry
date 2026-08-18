3e22ff1b9a75d054d53e062cc8f6240c08514ace
johannacastor <johanna_castor@dlsu.edu.ph>
2026-08-18 17:11:51 +0800

    feat(web): add login page Get endpoint #31

--- src/main/java/com/dlsu/animoregistry/web/AuthController.java
+++ src/main/java/com/dlsu/animoregistry/web/AuthController.java
@@ -1,8 +1,16 @@
 package com.dlsu.animoregistry.web;
 
+import com.dlsu.animoregistry.model.LasallianStudent;
+import com.dlsu.animoregistry.model.OrgOfficer;
 import com.dlsu.animoregistry.service.OrgOfficerService;
 import com.dlsu.animoregistry.service.StudentService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AuthController {
@@ -13,5 +21,11 @@ public class AuthController {
     public AuthController(StudentService studentService, OrgOfficerService officerService) {
         this.studentService = studentService;
         this.officerService = officerService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("title", "Log in");
+        return "login";
     }
 }
